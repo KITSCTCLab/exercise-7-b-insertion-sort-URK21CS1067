0ada692072dcc8e5bbce5b442831c738c2d09a45
URK21CS1067 <93753029+URK21CS1067@users.noreply.github.com>
2022-09-13 21:27:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,18 +2,18 @@ from typing import List
 
 def insertionSort(array) -> List[int]:
   # Write your code here
-  for i in range(1, len(arr)):
+  for i in range(1, len(data)):
   
-        key = arr[i]
+        key = data[i]
   
         # Move elements of arr[0..i-1], that are
         # greater than key, to one position ahead
         # of their current position
         j = i-1
-        while j >= 0 and key < arr[j] :
-                arr[j + 1] = arr[j]
+        while j >= 0 and key < data[j] :
+                data[j + 1] = data[j]
                 j -= 1
-        arr[j + 1] = key
+        data[j + 1] = key
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
